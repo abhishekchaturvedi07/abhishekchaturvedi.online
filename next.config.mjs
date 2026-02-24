a7b875c5ee772f31fdf86554aab88cd4a33cd036
Abhishek Sneha Chaturvedi <abhisheksnehachaturvedi@Abhisheks-MacBook-Pro.local>
2026-02-24 17:34:55 +0530

    remove custom distDir

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     // output: 'export', // Outputs a Single-Page Application (SPA).
+//     distDir: 'build', // Changes the build output directory to `./dist/`.
+//     images: {
+//       unoptimized: true,
+//     }
+//   }
+
+//   export default nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export', // Outputs a Single-Page Application (SPA).
-    distDir: 'build', // Changes the build output directory to `./dist/`.
-    images: {
-      unoptimized: true,
-    }
-  }
-   
-  export default nextConfig
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
